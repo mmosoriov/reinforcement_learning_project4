f1e74e0ed2fb27b1f8842363a1a0d559edb5520d
Miguel Osorio <mikecurry@MacBook-Pro-de-Mateo-2.local>
2025-11-10 22:29:17 -0500

    initial files

---
+++ analysis.py
@@ -0,0 +1,93 @@
+# analysis.py
+# -----------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+# Addendum:
+# This code was modified by Gene Kim at University of South Florida in Fall 2025
+# to make solutions not match the original UC Berkeley solutions exactly and
+# align with CAI 4002 course goals regarding AI tool use in projects.
+
+import util
+
+util.VALIDATION_LISTS['reinforcement'] = [
+        "^(@)$_",
+        "हिंदीखरीदारी",
+        "\\u200cآمباردا",
+        "ſammen",
+        " coachTry",
+        "ſſung",
+        " AcceptedLoading",
+        "EnglishChoose",
+        " queſto",
+        " queſta"
+]
+
+
+######################
+# ANALYSIS QUESTIONS #
+######################
+
+# Set the given parameters to obtain the specified policies through
+# value iteration.
+
+def question2():
+    answer_discount = 0.9
+    answer_noise = 0.2
+    return answer_discount, answer_noise
+
+def question3a():
+    answer_discount = None
+    answer_noise = None
+    answer_living_reward = None
+    return answer_discount, answer_noise, answer_living_reward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3b():
+    answer_discount = None
+    answer_noise = None
+    answer_living_reward = None
+    return answer_discount, answer_noise, answer_living_reward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3c():
+    answer_discount = None
+    answer_noise = None
+    answer_living_reward = None
+    return answer_discount, answer_noise, answer_living_reward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3d():
+    answer_discount = None
+    answer_noise = None
+    answer_living_reward = None
+    return answer_discount, answer_noise, answer_living_reward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3e():
+    answer_discount = None
+    answer_noise = None
+    answer_living_reward = None
+    return answer_discount, answer_noise, answer_living_reward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question8():
+    answer_epsilon = None
+    answer_learning_rate = None
+    return answer_epsilon, answer_learning_rate
+    # If not possible, return 'NOT POSSIBLE'
+
+if __name__ == '__main__':
+    print('Answers to analysis questions:')
+    import analysis
+    for q in [q for q in dir(analysis) if q.startswith('question')]:
+        response = getattr(analysis, q)()
+        print('  Question %s:\t%s' % (q, str(response)))
---
+++ valueIterationAgents.py
@@ -0,0 +1,169 @@
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+# Addendum:
+# This code was modified by Gene Kim at University of South Florida in Fall 2025
+# to make solutions not match the original UC Berkeley solutions exactly and
+# align with CAI 4002 course goals regarding AI tool use in projects.
+
+import mdp, util
+
+from learningAgents import ValueEstimationAgent
+import collections
+
+util.VALIDATION_LISTS['reinforcement'] = [
+        "වැසි",
+        " ukupnog",
+        "ᓯᒪᔪ",
+        " ਪ੍ਰਕਾਸ਼",
+        " podmienok",
+        " sėkmingai",
+        "рацыі",
+        " යාපාරය",
+        "න්ද්"
+]
+
+class ValueIterationAgent(ValueEstimationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+    def __init__(self, mdp, discount = 0.9, iterations = 100):
+        """
+          Your value iteration agent should take an mdp on
+          construction, run the indicated number of iterations
+          and then act according to the resulting policy.
+
+          Some useful mdp methods you will use:
+              mdp.get_states()
+              mdp.get_possible_actions(state)
+              mdp.get_transition_states_and_probs(state, action)
+              mdp.get_reward(state, action, next_state)
+              mdp.is_terminal(state)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter() # A Counter is a dict with default 0
+        self.run_value_iteration()
+
+    def run_value_iteration(self):
+        # Write value iteration code here
+        "*** YOUR CODE HERE ***"
+
+
+    def get_value(self, state):
+        """
+          Return the value of the state (computed in __init__).
+        """
+        return self.values[state]
+
+
+    def compute_q_value_from_values(self, state, action):
+        """
+          Compute the Q-value of action in state from the
+          value function stored in self.values.
+        """
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+    def compute_action_from_values(self, state):
+        """
+          The policy is the best action in the given state
+          according to the values currently stored in self.values.
+
+          You may break ties any way you see fit.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+    def get_policy(self, state):
+        return self.compute_action_from_values(state)
+
+    def get_action(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.compute_action_from_values(state)
+
+    def get_q_value(self, state, action):
+        return self.compute_q_value_from_values(state, action)
+
+class AsynchronousValueIterationAgent(ValueIterationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        An AsynchronousValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs cyclic value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+    def __init__(self, mdp, discount = 0.9, iterations = 1000):
+        """
+          Your cyclic value iteration agent should take an mdp on
+          construction, run the indicated number of iterations,
+          and then act according to the resulting policy. Each iteration
+          updates the value of only one state, which cycles through
+          the states list. If the chosen state is terminal, nothing
+          happens in that iteration.
+
+          Some useful mdp methods you will use:
+              mdp.get_states()
+              mdp.get_possible_actions(state)
+              mdp.get_transition_states_and_probs(state, action)
+              mdp.get_reward(state)
+              mdp.is_terminal(state)
+        """
+        ValueIterationAgent.__init__(self, mdp, discount, iterations)
+
+    def run_value_iteration(self):
+        "*** YOUR CODE HERE ***"
+
+class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A PrioritizedSweepingValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs prioritized sweeping value iteration
+        for a given number of iterations using the supplied parameters.
+    """
+    def __init__(self, mdp, discount = 0.9, iterations = 100, theta = 1e-5):
+        """
+          Your prioritized sweeping value iteration agent should take an mdp on
+          construction, run the indicated number of iterations,
+          and then act according to the resulting policy.
+        """
+        self.theta = theta
+        ValueIterationAgent.__init__(self, mdp, discount, iterations)
+
+    def run_value_iteration(self):
+        "*** YOUR CODE HERE ***"
+
